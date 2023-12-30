383115644bd4e905f8d96e1cae16f408bcd860e4
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-12-30 20:27:08 +0100

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,60 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignatire(): number {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignatire()} enters the house`);
+    } else {
+      console.log("The door is closed. Cannot enter");
+    }
+  }
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignatire() === this.key.getSignatire()) {
+      this.door = true;
+      console.log("Door is open");
+    } else {
+      console.log("invalid key. Door remains closed.");
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
